d4ccabaea6993d7969089e3e1810be0362ddbaf9
Akash Gupta <168327561+akash-cq@users.noreply.github.com>
2025-02-16 17:49:33 +0530

    Update footer.jsx

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Footer.module.css"; // Import CSS Module
+import styles from "./footer.module.css"; // Import CSS Module
 
 const Footer = () => {
   return (
